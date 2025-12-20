c1a60314c72b49afa9d7572f7a93063cabf40586
Matt Hammerly <hammerly.matt@gmail.com>
2025-12-19 17:55:30 -0800

    chore: add script for generating a new day's project

---
+++ scripts/new_day.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+day="day$1"
+
+repo_root=$(git rev-parse --show-toplevel)
+
+# go to git repository root to create our new day
+cd $repo_root
+
+# create the cargo project and fill in its `Cargo.toml`
+cargo new $day
+cat << EOF > $day/Cargo.toml
+[package]
+name = "$day"
+version = "0.1.0"
+edition = "2024"
+
+[lib]
+name = "$day"
+path = "src/lib.rs"
+
+[[bin]]
+name = "$day-1"
+path = "src/part_1.rs"
+
+[[bin]]
+name = "$day-2"
+path = "src/part_2.rs"
+
+[dependencies]
+anyhow = { workspace = true }
+thiserror = { workspace = true }
+tracing = { workspace = true }
+tracing-subscriber = { workspace = true }
+EOF
+
+# placeholder test files (need to fill in manually)
+touch $day/$day.input
+touch $day/$day.test.input
+
+# create starter `lib.rs`, `part_1.rs`, and `part_2.rs`
+touch $day/src/lib.rs
+cat << EOF > $day/src/part_1.rs
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, prelude::*};
+
+use $day::*;
+
+fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let input_file = File::open(format!("{}/$day.input", env!("CARGO_MANIFEST_DIR")))?;
+    let _reader = BufReader::new(input_file);
+
+    panic!("not implemented");
+}
+EOF
+cp $day/src/part_1.rs $day/src/part_2.rs
+
+# go back to wherever we were before
+cd -
